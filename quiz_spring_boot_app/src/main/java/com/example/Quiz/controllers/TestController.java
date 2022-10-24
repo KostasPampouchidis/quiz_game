b6ae84e640da2e3c0584c534210a15aa02e45cc2
KostasPampouchidis <91009141+KostasPampouchidis@users.noreply.github.com>
2022-10-24 09:20:27 +0300

    Add files via upload

---
+++ quiz_spring_boot_app/src/main/java/com/example/Quiz/controllers/TestController.java
@@ -0,0 +1,65 @@
+package com.example.Quiz.controllers;
+
+import com.example.Quiz.dtos.AnswerDto;
+import com.example.Quiz.models.Answer;
+import com.example.Quiz.models.Question;
+import com.example.Quiz.services.AnswerServiceInterface;
+import com.example.Quiz.services.QuestionServiceInterface;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+  
+  @Autowired
+  AnswerServiceInterface answerServiceInterface;     
+  
+  @Autowired
+  QuestionServiceInterface questionServiceInterface;     
+    
+  @GetMapping("/getallquestions")
+  public List<Question> getAllQuestions() {
+      List<Question> questionList = new ArrayList<>();
+      questionList = questionServiceInterface.getAllQuestions();
+    return questionList;
+  }
+  
+  @GetMapping("/getallanswers")
+  public List<AnswerDto> getAllAnswers() {
+      List<Answer> answerList = new ArrayList<>();
+      List<AnswerDto> answerDtoList = new ArrayList<>();
+      
+      answerList = answerServiceInterface.getAllAnswers();
+      
+      for(Answer a: answerList){
+          answerDtoList.add(new AnswerDto(a));
+      }
+    return answerDtoList;
+  }
+  
+  @GetMapping("/all")
+  public String allAccess() {
+    return "Public Content.";
+  }
+
+  @GetMapping("/user")
+  @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+  public String userAccess() {
+    return "User Content.";
+  }
+
+  @GetMapping("/admin")
+  @PreAuthorize("hasRole('ADMIN')")
+  public String adminAccess() {
+    return "Admin Board.";
+  }
+  
+}
